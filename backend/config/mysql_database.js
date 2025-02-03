debfe6d9a9010a7c34246ad2c22dd3f7db83621d
Arjun Singh Naruka <89446608+arjunji123@users.noreply.github.com>
2025-02-03 15:04:46 +0530

    Update mysql_database.js

--- backend/config/mysql_database.js
+++ backend/config/mysql_database.js
@@ -3,10 +3,10 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "backend/config/config.env" });
 
 const mysqlPool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "stakenew",
+  host: "b8hx39ruavxxcr4ikqzl-mysql.services.clever-cloud.com",
+  user: "udolchrwbssth4w7",
+  password: "2Di5RDyWUoSAyJ0LupUj",
+  database: "b8hx39ruavxxcr4ikqzl",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
